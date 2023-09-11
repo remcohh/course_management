840152e63d740b1c5610d1ee19a2bb6e062bc556
Remco Huijdts <r.huijdts@maastrichtuniversity.nl>
2023-09-11 11:58:37 +0200

    use many2many for grades and tests

--- models/grading_profile.py
+++ models/grading_profile.py
@@ -3,7 +3,7 @@ from odoo import fields, models
 class CmGradingProfile(models.Model):
     _name = "cm.grading_profile"
     product_templates = fields.One2many("product.template", "grading_profile")
-    tests = fields.One2many("cm.test", "grading_profile")
+    tests = fields.Many2many("cm.test")
     name = fields.Char()
     allowed_absence_sessions = fields.Integer()
     
--- models/test.py
+++ models/test.py
@@ -3,7 +3,6 @@ from odoo import fields, models
 class CmTest(models.Model):
     _name = "cm.test"   
     _rec_name = 'title'
-    grading_profile = fields.Many2one("cm.grading_profile")
+    grading_profiles = fields.Many2many("cm.grading_profile")
     title = fields.Char()
     description = fields.Html("description")
-    
